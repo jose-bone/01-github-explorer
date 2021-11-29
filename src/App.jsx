bcb1033e894280a657c70a6f0e29d650bfedfe1f
José Boné <79787418+jose-bone@users.noreply.github.com>
2021-11-29 18:17:56 +0200

    Webpack Dev Server

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export function App() {
-  return <h1>Hello World!!🖖🏾</h1>;
+  return <h1>Hello World!🖖🏾</h1>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
